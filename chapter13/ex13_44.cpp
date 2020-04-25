5750ad7191a6c2e4dad9a23a1c35814a2414a6d3
wangheng <wh9697yyk@163.com>
2020-04-26 01:25:03 +0800

    添加了练习13.44的源码

--- chapter13/ex13_44.cpp
+++ chapter13/ex13_44.cpp
@@ -2,11 +2,17 @@
 // Created by wangheng on 2020/4/26.
 //
 
-#include <string>
 #include <iostream>
+#include "ex13_44.h"
 
 int main() {
-    std::string str("hello");
-    std::cout << str[1] << std::endl;
+    String str("hello");
+    for (auto iter = str.begin(); iter != str.end(); ++iter)
+        std::cout << *iter << '\t';
+    std::cout << std::endl;
+    std::cout << str.c_str() << std::endl;
+    std::cout << str.front() << '\t' << str.back() << '\t' << str[1] << std::endl;
+    String str2(str);
+    std::cout << str2.c_str() << std::endl;
     return 0;
 }
--- chapter13/ex13_44.h
+++ chapter13/ex13_44.h
@@ -6,17 +6,68 @@
 #define CPP_PRIMER_EX13_44_H
 
 #include <memory>
+#include <cstring>
+#include <algorithm>
 
 class String {
 public:
-    String() {}
-    
+    String() : elements(nullptr), cap(nullptr) {}
+    String(const char*);
+    String(const String&);
+    ~String();
+    char operator[](std::size_t) const ;
+    char front() const { return *elements; }
+    char back() const { return *(cap - 1);}
+    const char *c_str();
+    std::size_t size() const { return cap - elements; }
+    char *begin() const { return elements; }
+    char *end() const { return cap; }
+
 private:
+    static std::allocator<char> alloc;
     std::pair<char*, char*> alloc_n_copy(const char*, const char*);
-    void range_initializer(const char*, const char*);
     void free();
     char *elements;
-    char *end;
-    std::allocator<char> alloc;
+    char *cap;
 };
+
+std::allocator<char> String::alloc;
+
+String::String(const char *pc) {
+    auto newdata = alloc.allocate(std::strlen(pc));
+    elements = newdata;
+    auto len = std::strlen(pc);
+    for (std::size_t i = 0; i < len; ++i)
+        alloc.construct(newdata++, *pc++);
+    cap = newdata;
+}
+
+String::String(const String &s) {
+    auto newdata = alloc_n_copy(s.begin(), s.end());
+    elements = newdata.first;
+    cap = newdata.second;
+}
+
+String::~String() { free(); }
+
+void String::free() {
+    if (elements) {
+        std::for_each(begin(), end(), [](const char& c) {alloc.destroy(&c);});
+        alloc.deallocate(begin(), size());
+    }
+}
+
+const char* String::c_str() {
+    return elements;
+}
+
+char String::operator[](std::size_t i) const {
+    return *(elements+i);
+}
+
+std::pair<char*, char*> String::alloc_n_copy(const char *b, const char *e) {
+    auto data = alloc.allocate(e - b);
+    return {data, std::uninitialized_copy(b, e, data)};
+}
+
 #endif //CPP_PRIMER_EX13_44_H
