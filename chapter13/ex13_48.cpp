3168aaa0989a211c0c6aa4c9c096f877b008dac2
wangheng <wh9697yyk@163.com>
2020-04-26 11:41:25 +0800

    创建了练习13.48的源码

---
+++ chapter13/ex13_48.cpp
@@ -0,0 +1,18 @@
+//
+// Created by wangheng on 2020/4/26.
+//
+
+#include <iostream>
+#include <vector>
+#include "ex13_44.h"
+
+int main() {
+    std::vector<String> strVec;
+    strVec.push_back("hello");
+    strVec.push_back("good");
+    strVec.push_back("world");
+    strVec.push_back("china");
+    strVec.push_back("cpp");
+
+    return 0;
+}
