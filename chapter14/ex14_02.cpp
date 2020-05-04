06a246b15e6dacc20c4ca4b1006d9063c7e0586c
wangheng <wh9697yyk@163.com>
2020-05-04 18:28:37 +0800

    新建了14.02题的源文件

---
+++ chapter14/ex14_02.cpp
@@ -0,0 +1,46 @@
+//
+// Created by wangheng on 2020/5/4.
+//
+
+#include "ex14_02.h"
+
+Sales_data::Sales_data(std::istream &is) {
+    is >> *this;
+}
+
+Sales_data& Sales_data::operator+=(const Sales_data &rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+std::istream& operator>>(std::istream& is, Sales_data& item) {
+    double price = 0.0;
+    is >> item.bookNo >> item.units_sold >> price;
+    if (is) {
+        item.revenue = price * item.units_sold;
+    } else {
+        item = Sales_data();
+    }
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, Sales_data& item) {
+    os << item.isbn() << " " << item.units_sold << " " << item.revenue << " " << item.avg_price();
+    return os;
+}
+
+Sales_data operator+(const Sales_data& lhs, const Sales_data& rhs) {
+    Sales_data sum = lhs;
+    sum += rhs;
+    return sum;
+}
+
+int main() {
+    Sales_data item1(std::cin);
+    Sales_data item2("2", 3, 3.4);
+    Sales_data sum = item1 + item2;
+    std::cout << sum << std::endl;
+
+    return 0;
+}
---
+++ chapter14/ex14_02.h
@@ -0,0 +1,43 @@
+//
+// Created by wangheng on 2020/5/4.
+//
+
+#ifndef CPP_PRIMER_EX14_02_H
+#define CPP_PRIMER_EX14_02_H
+
+#include <string>
+#include <iostream>
+#include <utility>
+
+class Sales_data {
+    friend std::istream& operator>>(std::istream&, Sales_data&);    // 输入
+    friend std::ostream& operator<<(std::ostream&, Sales_data&);    // 输出
+    friend Sales_data operator+(const Sales_data&, const Sales_data&);  // 加法
+public:
+    Sales_data(std::string  s, unsigned n, double p) :
+        bookNo(std::move(s)), units_sold(n), revenue(n * p){}
+    Sales_data() : Sales_data("", 0, 0.0f) {}
+    Sales_data(const std::string& s) : Sales_data(s, 0, 0.0f) {}
+    Sales_data(std::istream& is);
+
+    Sales_data& operator+=(const Sales_data&);  // 复合运算符
+    std::string isbn() const {return bookNo;}
+
+private:
+    inline double avg_price() const ;
+
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+std::istream& operator>>(std::istream&, Sales_data&);
+std::ostream& operator<<(std::ostream&, Sales_data&);
+Sales_data operator+(const Sales_data&, const Sales_data&);
+
+inline
+double Sales_data::avg_price() const {
+    return units_sold ? revenue / units_sold : 0;
+}
+
+#endif //CPP_PRIMER_EX14_02_H
