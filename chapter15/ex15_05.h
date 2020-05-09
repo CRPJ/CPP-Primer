caa1ead75fcad77d1c986f02390ac2b3517a1515
wangheng <wh9697yyk@163.com>
2020-05-09 11:22:03 +0800

    为练习15.05添加了源代码

---
+++ chapter15/ex15_05.h
@@ -0,0 +1,29 @@
+//
+// Created by wangheng on 2020/5/9.
+//
+
+#ifndef CPP_PRIMER_EX15_05_H
+#define CPP_PRIMER_EX15_05_H
+
+#include "ex15_03.h"
+
+class Bulk_quote : public Quote {
+public:
+    Bulk_quote() = default;
+    Bulk_quote(const std::string& book, double p, std::size_t qty, double disc) :
+        Quote(book, p), min_qty(qty), discount(disc) {}
+    double net_price(std::size_t) const override ;
+
+private:
+    std::size_t min_qty = 0;
+    double discount = 0.0;
+};
+
+double Bulk_quote::net_price(std::size_t cnt) const {
+    if (cnt >= min_qty)
+        return cnt * (1 - discount) * price;
+    else
+        return cnt * price;
+}
+
+#endif //CPP_PRIMER_EX15_05_H
