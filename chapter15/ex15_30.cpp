37c0d3852a4fadbd78cb35f9b4c469c7fa0abdfe
wangheng <wh9697yyk@163.com>
2020-06-07 19:29:24 +0800

    添加了15.30的答案

--- chapter15/ex15_30.cpp
+++ chapter15/ex15_30.cpp
@@ -10,6 +10,8 @@ int main() {
     Basket basket;
     basket.add_item(Bulk_quote("1234", 4.5, 100, 0.2));
     basket.add_item(Quote("12321", 5.6));
+    Bulk_quote item("1234", 4.5, 100, 0.2);
+    basket.add_item(item);
     basket.total_receipt(std::cout);
 
     return 0;
