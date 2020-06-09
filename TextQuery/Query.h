0ee4ddd5b86085ffa52347a2e95e70c369c23381
wangheng <wh9697yyk@163.com>
2020-06-09 14:40:21 +0800

    给Query类添加了<<重载运算符打印运算表达式

--- TextQuery/Query.h
+++ TextQuery/Query.h
@@ -16,6 +16,7 @@ class Query : public Query_base {
     friend Query operator~(const Query&);
     friend Query operator|(const Query&, const Query&);
     friend Query operator&(const Query&, const Query&);
+    friend std::ostream& operator<<(std::ostream&, const Query&);
 
 public:
     explicit Query(const std::string& s);
@@ -33,5 +34,8 @@ private:
     std::shared_ptr<Query_base> q;
 };
 
+inline std::ostream& operator<<(std::ostream& os, const Query& query) {
+    return os << query.rep();
+}
 
 #endif //QUERY_QUERY_H
