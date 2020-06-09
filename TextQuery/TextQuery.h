5d3997987e322dff983715a324c996076c9caf02
wangheng <wh9697yyk@163.com>
2020-06-09 10:37:10 +0800

    添加注释

--- TextQuery/TextQuery.h
+++ TextQuery/TextQuery.h
@@ -21,7 +21,10 @@ public:
     QueryResult query(const std::string&) const ;
 
 private:
+    // file保存查询的源文本内容，安行保存
     std::shared_ptr<std::vector<std::string>> file;
+    // 将每个单词和对应出现的行号组成的set构成一个map，通过输入需要查询的单词可以得到对应出
+    // 现的行号的set
     std::map<std::string, std::shared_ptr<std::set<line_no>>> wm;
 };
 
